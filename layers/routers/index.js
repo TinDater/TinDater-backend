d267bdd6bf8cc167bb82850f1bf98dbb1d545a42
jc <codeing999@gmail.com>
2022-08-20 01:05:06 +0900

    add config.js, delete config.json

--- layers/routers/index.js
+++ layers/routers/index.js
@@ -5,8 +5,8 @@ const authRouter = require("./auth.router");
 const userRouter = require("./user.router");
 const peopleRouter = require("./people.router");
 
-router.use("/auth", authRouter);
-router.use("/user", userRouter);
+// router.use("/auth", authRouter);
+// router.use("/user", userRouter);
 router.use("/people", peopleRouter);
 
 module.exports = router;
